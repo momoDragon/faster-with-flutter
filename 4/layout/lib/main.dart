c8426ecf650d173fa698ce9752da180a4b0d271e
momoDragon <mozesong350@gmail.com>
2023-04-18 10:15:09 +0800

    4

---
+++ 4/layout/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RowPage(),
+    );
+  }
+}
+
+class ColumnPage extends StatefulWidget {
+  const ColumnPage({super.key});
+
+  @override
+  State<ColumnPage> createState() => _ColumnPageState();
+}
+
+class _ColumnPageState extends State<ColumnPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.black,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, // vertical
+          crossAxisAlignment: CrossAxisAlignment.center, // horizontal
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class RowPage extends StatefulWidget {
+  const RowPage({super.key});
+
+  @override
+  State<RowPage> createState() => _RowPageState();
+}
+
+class _RowPageState extends State<RowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.black,
+        height: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // horizontal
+          crossAxisAlignment: CrossAxisAlignment.center, // veritical
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
